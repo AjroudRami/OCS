da7e90e645db0beaab7705a2dfd331bad8af4232
Ajroud Rami <ajroud.rami@hotmail.fr>
2017-10-31 14:33:18 +0100

    Update proximity service
    Add endpoint to get orientation

--- Software/services/proximity/src/main/java/ProximityService.java
+++ Software/services/proximity/src/main/java/ProximityService.java
@@ -1,2 +1,30 @@
+import javax.ejb.EJB;
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+@Path("/proximity/")
+@Consumes(MediaType.APPLICATION_JSON)
 public class ProximityService {
+    private Logger LOGGER = Logger.getLogger(this.getClass().getSimpleName());
+    @EJB
+    private ArduinoBoard board;
+
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public Orientation getOrientation() {
+        Orientation orientation = new Orientation();
+        try {
+            LOGGER.log(Level.INFO, "Received Orientation request");
+            orientation = board.requestYPRAndWait(500);
+            LOGGER.log(Level.INFO, "Successfully received orientation from sensor: " + orientation.toString());
+        } catch (Exception e) {
+            LOGGER.log(Level.INFO, "Exception sent " + e.getMessage());
+            e.printStackTrace();
+        }
+        return orientation;
+    }
 }
