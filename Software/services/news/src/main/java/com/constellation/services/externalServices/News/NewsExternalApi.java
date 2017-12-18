5b4507eec1bae745efd22f090746a355059b22c9
Ajroud Rami <ajroud.rami@hotmail.fr>
2017-12-18 08:47:39 +0100

    Project cleanup

--- Software/services/news/src/main/java/com/constellation/services/externalServices/News/NewsExternalApi.java
+++
@@ -1,9 +0,0 @@
-package com.constellation.services.externalServices.News;
-
-import io.swagger.model.News;
-
-import java.util.List;
-
-public interface NewsExternalApi {
-  public List<News> getNewsByCountryName(String countryName, String lang);
-}
