c50cf04460e380bbcacf8a2219abb9eaad692d23
Ajroud Rami <ajroud.rami@hotmail.fr>
2018-01-28 04:30:00 +0100

    Arduino firmware led commands implementation

--- Software/ArduinoFirmware/Firmware/FirmwareV2.ino/FirmwareV2.ino.ino
+++ Software/ArduinoFirmware/Firmware/FirmwareV2.ino/FirmwareV2.ino.ino
@@ -1,11 +1,23 @@
 #include <SoftwareSerial.h>
  
+
 SoftwareSerial mySerial(10, 11); // RX pin on arduino pin 10, TX pin on arduino pin 11
+
+#define LED_RED 3
+#define LED_GREEN 5
+#define LED_BLUE 6
+
 boolean commandReceived = false;
 byte commandBuff[64];
 int commandLength;
 
+boolean streamYPR = false;
+
 void setup() {
+  pinMode(LED_RED, OUTPUT);
+  pinMode(LED_GREEN, OUTPUT);
+  pinMode(LED_BLUE, OUTPUT);
+
   mySerial.begin(9600);
 
 }
@@ -13,7 +25,7 @@ void setup() {
 void loop() {
   pollCommand();
   executeCommand();
-
+  stream();
 }
 
 // Empty the Serial buffer and fill the commandBuffer
@@ -25,11 +37,14 @@ void pollCommand(){
       commandBuff[i] = received;
     }
     commandReceived = true;
+  } else {
+    commandReceived = false;
   }
 }
 
 void executeCommand(){
-  switch(commandBuffer[0]){
+  if(!commandReceived){ return; }
+  switch(commandBuff[0]){
     case 10: commandYPR();
              break;
     case 11: commandStreamYPR();
@@ -38,23 +53,40 @@ void executeCommand(){
              break;
     case 13: commandLedOff();
              break;
+    default: break;
   }
   commandReceived = false;
 }
 
 void commandYPR(){
-  
+  byte response[9];
 }
 
 void commandStreamYPR(){
-  
+  byte activeStream = commandBuff[2];
+  if (activeStream) {
+    streamYPR = true;
+  } else {
+    streamYPR = false;
+  }
 }
 
 void commandLedOn(){
-  
+  byte red = commandBuff[3];
+  byte green = commandBuff[4];
+  byte blue = commandBuff[5];
+  analogWrite(LED_RED, red);
+  analogWrite(LED_GREEN, green);
+  analogWrite(LED_BLUE, blue);
 }
 
 void commandLedOff(){
+  analogWrite(LED_RED, 0);
+  analogWrite(LED_GREEN, 0);
+  analogWrite(LED_BLUE, 0);
+}
+
+void stream(){
   
 }
 
