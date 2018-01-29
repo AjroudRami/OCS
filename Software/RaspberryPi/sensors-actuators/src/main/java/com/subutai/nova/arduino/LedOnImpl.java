a25da0d460060fc4c9af46128879bb80a403011a
Rami <ajroud.rami@hotmail.fr>
2018-01-29 06:34:23 +0100

    Add log

--- Software/RaspberryPi/sensors-actuators/src/main/java/com/subutai/nova/arduino/LedOnImpl.java
+++ Software/RaspberryPi/sensors-actuators/src/main/java/com/subutai/nova/arduino/LedOnImpl.java
@@ -3,14 +3,18 @@ package com.subutai.nova.arduino;
 import javax.ejb.EJB;
 import javax.ejb.Stateless;
 import javax.ws.rs.core.Response;
+import java.util.logging.Logger;
 
 @Stateless
 public class LedOnImpl implements LedOn {
+
+    private static Logger LOGGER = Logger.getLogger(LedOnImpl.class.getName());
 
     @EJB
     ArduinoCommander commander;
 
     public Response computePath() {
+        LOGGER.info("commander = " + commander);
         commander.sendCommand(new com.subutai.nova.arduino.command.list.LedOn(java.awt.Color.RED.getRGB()));
         return Response.ok().build();
     }
