c2ec16096170f59efe6b884973587e92cb2f7f2e
Rami <ajroud.rami@hotmail.fr>
2018-02-06 15:39:39 +0100

    BugFix in Callbackregister
    Make sure to implements the Arduino communication protocol we choose
    1 byte for the commandId
    1 byte for the command length
    2 bytes for the callback id
    
    this resulted in reading a bad callback id (not registered) and leading
    to a nullpointerexception
    Firmware update comming

--- Software/RaspberryPi/sensors-actuators/src/main/java/com/subutai/nova/arduino/command/CommandResponse.java
+++ Software/RaspberryPi/sensors-actuators/src/main/java/com/subutai/nova/arduino/command/CommandResponse.java
@@ -8,14 +8,16 @@ public class CommandResponse {
     private byte id;
     private short callbackId;
     private byte[] bytes;
+    private byte length;
 
     public static CommandResponse fromBytes(byte[] bytes) {
         CommandResponse response = new CommandResponse();
         response.id = bytes[0];
-        response.callbackId = BinaryConverter.getShort(new byte[]{bytes[1], bytes[2]});
-        byte[] payload = new byte[bytes.length - 3];
+        response.length = bytes[1];
+        response.callbackId = BinaryConverter.getShort(new byte[]{bytes[2], bytes[3]});
+        byte[] payload = new byte[bytes.length - 4];
         response.bytes = payload;
-        ByteArrayUtil.copy(bytes, response.bytes, 3, 0, response.bytes.length);
+        ByteArrayUtil.copy(bytes, response.bytes, 4, 0, response.bytes.length);
         return response;
     }
 
