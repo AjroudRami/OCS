a441a48f82e3746ea494a2c602f15a18fc036d1b
Rami <ajroud.rami@hotmail.fr>
2018-02-06 02:02:09 +0100

    Wrong path for orientation

--- Software/RaspberryPi/sensors-actuators/src/main/java/com/subutai/nova/arduino/ws/OrientationWS.java
+++ Software/RaspberryPi/sensors-actuators/src/main/java/com/subutai/nova/arduino/ws/OrientationWS.java
@@ -7,7 +7,7 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
-@Path("/ledOn")
+@Path("/orientation")
 @Consumes(MediaType.APPLICATION_JSON)
 @Produces(MediaType.APPLICATION_JSON)
 public interface OrientationWS {
