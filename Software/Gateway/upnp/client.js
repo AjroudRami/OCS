f715fd422b3e5fef15a8db877f1c28444eb6f284
Marhold Mônier <marhold.monier@gmail.com>
2018-02-19 13:33:41 +0100

    nodejs client upnp

--- Software/Gateway/upnp/client.js
+++ Software/Gateway/upnp/client.js
@@ -2,6 +2,8 @@ const ssdp = require('node-ssdp').Client;
 const client = new ssdp({});
 
 let nbResponses = 0;
+let address;
+let port;
 
 client.on('notify', function () {
   // console.log('Got a notification.')
@@ -9,10 +11,19 @@ client.on('notify', function () {
 
 client.on('response', function inResponse(headers, code, rinfo) {
   nbResponses += 1;
-  console.log(`Got a response to an m-search: 
+  /*console.log(`Got a response to an m-search: 
     ${code},
     ${JSON.stringify(headers)},
-    ${JSON.stringify(rinfo)}.`);
+    ${JSON.stringify(rinfo)}.`);*/
+  if(headers.USN === 'uuid:93691f7e-9962-1e85-ffff-ffffc4ed5d70::urn:schemas-upnp-org:service:EarthService:1') {
+    address = rinfo.address;
+    port = rinfo.port;
+  } else {
+    console.log('headers.USN:');
+    console.log(headers.USN);
+    console.log('JSON.parse(headers).USN:');
+    console.log(JSON.parse(headers).USN);
+  }
 })
 
 // client.search('urn:schemas-upnp-org:service:ContentDirectory:1');
