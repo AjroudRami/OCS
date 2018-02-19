9be8fd873a02d42d1f1d3d2e37c65f9589c07ed3
Marhold Mônier <marhold.monier@gmail.com>
2018-02-19 23:19:01 +0100

    add interface / http requests / sockets

--- Software/Gateway/upnp/client.js
+++ Software/Gateway/upnp/client.js
@@ -1,35 +1,90 @@
+const express = require('express');
+const app = express();
 const ssdp = require('node-ssdp').Client;
 const client = new ssdp({});
+const axios = require('axios');
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
 
-let nbResponses = 0;
+let discovered = false;
 let address;
 let port;
+let url;
+
+const path = '/sensors-actuators-1.0-SNAPSHOT';
+
+app.get('/', function(req, res) {
+  res.sendFile('./index.html', { root: '.'});
+});
+
+io.on('connection', function (socket) {
+  console.log('user connected');
+
+  if (discovered) {
+    socket.emit('state', { state: 'ready' });
+  }
+
+  socket.on('message', function(req) {
+    app.post('/message', function(req, res) {
+      axios.post(`${url}/message`, {
+        msg: req.msg
+      })
+      .then((resp) => {
+        // TODO
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+    });
+  });
+
+  socket.on('orientation', function(req) {
+    axios.get(`${url}/orientation`)
+         .then((response) => {
+           // TODO
+    });
+  });
+
+  socket.on('ledOn', function(req) {
+    console.log(req.color);
+    /*
+    axios.post(`${url}/ledOn`, {
+      red: req.color.red,
+      green: req.color.green,
+      blue: req.color.blue,
+    })
+    .then((resp) => {
+      // TODO
+    })
+    .catch((err) => {
+      console.error(err);
+    });
+    */
+  });
+});
+
+http.listen(8000, function(){
+  console.log('listening on *:8000');
+});
 
 client.on('notify', function () {
-  // console.log('Got a notification.')
+  console.log('Got a notification.')
 })
 
 client.on('response', function inResponse(headers, code, rinfo) {
-  nbResponses += 1;
-  /*console.log(`Got a response to an m-search: 
-    ${code},
-    ${JSON.stringify(headers)},
-    ${JSON.stringify(rinfo)}.`);*/
-  if(headers.USN === 'uuid:93691f7e-9962-1e85-ffff-ffffc4ed5d70::urn:schemas-upnp-org:service:EarthService:1') {
-    address = rinfo.address;
-    port = rinfo.port;
-  } else {
-    console.log('headers.USN:');
-    console.log(headers.USN);
-    console.log('JSON.parse(headers).USN:');
-    console.log(JSON.parse(headers).USN);
-  }
-})
+  address = rinfo.address;
+  port = rinfo.port;
+  url = `http://${address}:${port}${path}`;
+  discovered = true;
+  io.emit('state', { state: 'ready' });
+  console.log(`EarthService fount at: http://${address}:${port}/`);
+});
 
-// client.search('urn:schemas-upnp-org:service:ContentDirectory:1');
-client.search('ssdp:all');
+console.log('ssdp scan started');
+client.search('urn:schemas-upnp-org:service:EarthService:1');
+// client.search('ssdp:all');
 
 setTimeout(() => {
-  console.log(`GOT ${nbResponses} RESPONSE(S)`);
+  console.log(`ssdp scan stopped`);
   client.stop()
 }, 5000)
